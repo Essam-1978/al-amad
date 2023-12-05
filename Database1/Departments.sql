89941dd9f227cd87cbc03ba92fb0d24cc749ad83
Essam-1978 <essam.alibrahimi@gmail.com>
2023-12-05 20:33:36 +0300

    create emp dep tables

---
+++ Database1/Departments.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Departments]
+(
+	[Dep_Id] INT NOT NULL PRIMARY KEY, 
+    [Dep_Name] NCHAR(10) NOT NULL
+)
--- Database1/employees.sql
+++ Database1/employees.sql
@@ -1,4 +1,14 @@
 ﻿CREATE TABLE [dbo].[employees]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Emp_Id] INT NOT NULL PRIMARY KEY, 
+    [Ftrst_Name] NCHAR(10) NOT NULL, 
+    [Mid_Name] NCHAR(10) NOT NULL, 
+    [Last_Name] NCHAR(10) NOT NULL, 
+	[Birth_Date] Date NOT NULL,
+    [Photo] BINARY(50) NOT NULL, 
+    [Badge_Id] BINARY(50) NULL, 
+    [Dep_Id] INT NOT NULL, 
+    [Address] NCHAR(10) NULL, 
+    [Mobile] NCHAR(10) NULL, 
+    [Hire_Date] DATE NOT NULL
 )
