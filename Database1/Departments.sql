da0765d4a84e589498214e4d1495349be606df33
Essam-1978 <essam.alibrahimi@gmail.com>
2023-12-08 11:19:29 +0300

    create employee and dep tables

--- Database1/Departments.sql
+++ Database1/Departments.sql
@@ -1,4 +1,5 @@
-﻿CREATE TABLE [dbo].[Departments]
+﻿
+CREATE TABLE [dbo].[Departments]
 (
 	[Dep_Id] INT NOT NULL PRIMARY KEY, 
     [Dep_Name] NCHAR(10) NOT NULL
--- Database1/employees.sql
+++ Database1/employees.sql
@@ -1,14 +1,14 @@
 ﻿CREATE TABLE [dbo].[employees]
 (
 	[Emp_Id] INT NOT NULL PRIMARY KEY, 
-    [Ftrst_Name] NCHAR(10) NOT NULL, 
-    [Mid_Name] NCHAR(10) NOT NULL, 
-    [Last_Name] NCHAR(10) NOT NULL, 
-	[Birth_Date] Date NOT NULL,
+    [Ftrst_Name] NCHAR(50) NOT NULL, 
+    [Mid_Name] NCHAR(50) NOT NULL, 
+    [Last_Name] NCHAR(50) NOT NULL, 
+	[Birth_Date] DATETIME NOT NULL,
     [Photo] BINARY(50) NOT NULL, 
     [Badge_Id] BINARY(50) NULL, 
     [Dep_Id] INT NOT NULL, 
-    [Address] NCHAR(10) NULL, 
-    [Mobile] NCHAR(10) NULL, 
+    [Address] NCHAR(50) NULL, 
+    [Mobile] NCHAR(20) NULL, 
     [Hire_Date] DATE NOT NULL
 )
